d96c9310d6fd3e687fc9fe2a67894563806e6028
tanvi1978 <69166702+tanvi1978@users.noreply.github.com>
2022-03-30 20:58:28 -0700

    Update class94.py

--- class94.py
+++ class94.py
@@ -5,7 +5,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.svm import *
+#from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
